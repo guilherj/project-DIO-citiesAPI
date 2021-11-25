f91a3119735441f9e64f2b5ceab8db56ec11c62f
guilherj <contato.guilhermenascimento@hotmail.com>
2021-11-25 09:20:45 -0300

    Implemented method findById with treatment of HTTP Status

--- src/main/java/me/dio/citiesapi/CountryResource.java
+++ src/main/java/me/dio/citiesapi/CountryResource.java
@@ -3,11 +3,15 @@ package me.dio.citiesapi;
 import me.dio.citiesapi.countries.Country;
 import me.dio.citiesapi.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -18,9 +22,22 @@ public class CountryResource {
     private CountryRepository repository;
 
     @GetMapping
-    public List<Country> countries(){
+    public Page<Country> countries(Pageable page){
 
-        return repository.findAll();
+        return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getOne(@PathVariable Long id){
+        Optional<Country> optional = repository.findById(id);
+
+        if(optional.isPresent()){
+            return ResponseEntity.ok().body(optional.get());
+
+        } else{
+            return ResponseEntity.notFound().build();
+        }
+
     }
 
 }
